7e1bfb2cf6b73a97ce245c511f1490c55017038e
Faizan Jan <faizanjan619@gmail.com>
2023-01-26 01:07:36 +0530

    Complete the movie page and add the like unlike functionality

--- moviePage.js
+++ moviePage.js
@@ -1,52 +1,78 @@
 let THE_MOVIE = localStorage.getItem("THE_MOVIE");
+let favorites = JSON.parse(localStorage.getItem("favoriteMovies"));
 
 (async function loadMovie(){
+
+    document.querySelector("title").innerText=THE_MOVIE;
+
     let movie = await fetchMovies(THE_MOVIE);
-    // console.log(movie.Title, movie);
-//     let display = document.createElement("div");
-//     // console.log(movie);
-//     display.classList.add("w-75", "h-50")
-//     display.innerHTML = `
-//     <div id="fav-movie-card" class="card m-3 bg-warning text-dark m-auto mt-5">
-//         <div class="row g-0 d-flex align-items-around">
-//             <div class="image-container col-md-4 d-flex align-items-center" >
-//                 <a class="position-relative play-on-hover play" href="#">
-//                     <img class="img-fluid latest-release-image" src="${movie.Poster}"
-//                         class="img-fluid rounded-start" alt="Poster not available" />
-//                 </a>
-//             </div>
-//             <div class="col-md-7 m-2">
-//                 <div class="card-body d-flex flex-column justify-content-around align-items-around h-100">
 
-//                     <i id="add-to-fav" title="Add to Favorites"
-//                         class="fa-regular text-light fa-heart mt-1 mb-3 fs-3 align-self-end"></i>
-//                     <h1 class="card-title">${movie.Title}</h1>
-//                     <span class="card-text d-flex flex-column justify-content-center">
-//                         <p class="release-date text-grey fs-4">
-//                             ${movie.Year}
-//                         </p>
-//                         <!-- <p class="actors text-light fs-4"> ${movie.Actors} </p>-->
-//                         <p class="plot fs-5 text-light">${movie.Plot}</p>
-//                     </span>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-//     `
-// let main = document.getElementsByTagName("main")[0];
-// main.appendChild(display);
+    document.querySelector(".movie-title").innerText= "  "+movie.Title;
+    document.querySelector(".movie-year").innerText="  "+movie.Year;
+    document.querySelector(".movie-plot").innerText="  "+movie.Plot;
+    document.querySelector(".movie-actors").innerText="  "+movie.Actors;
+    document.querySelector(".movie-genre").innerText="  "+movie.Genre;
+    document.querySelector(".movie-type").innerText="  "+movie.Type;
+    document.querySelector(".movie-director").innerText="  "+movie.Director;
+    document.querySelector(".movie-released").innerText="  "+movie.Released;
+    document.querySelector(".movie-runtime").innerText="  "+movie.Runtime;
+    document.querySelector(".movie-rating").innerText="  "+movie.imdbRating;
+    document.querySelector(".movie-poster").setAttribute('src', movie.Poster)
 
-    let movieTitle = document.querySelector(".movie-title")
-    let movieYear = document.querySelector(".movie-year")
-    let moviePlot = document.querySelector(".movie-plot")
-    let movieActors = document.querySelector(".movie-actors")
-    let moviePoster = document.querySelector(".movie-poster")
+    // HANDLING FAVORITE BUTTON
+    function handleFavBtn() {
+        let addToFavBtn = document.getElementById("add-to-fav");
 
-    movieTitle.innerText= movie.Title;
-    movieYear.innerText=movie.Year;
-    moviePlot.innerText=movie.Plot;
-    moviePoster.setAttribute('src', movie.Poster)
-    movieActors.innerText=(movie.Actors);
-    console.log(movie.Actors);
+        addToFavBtn.addEventListener("mouseover", mouseInOnFav)
+        function mouseInOnFav() {
+            addToFavBtn.classList.remove("fa-regular");
+            addToFavBtn.classList.add("fa-solid");
+        }
+
+        addToFavBtn.addEventListener("mouseout", mouseOutOnFav)
+        function mouseOutOnFav() {
+            addToFavBtn.classList.remove("fa-solid");
+            addToFavBtn.classList.add("fa-regular");
+        }
+
+        let isFav = false;
+        for (let m of favorites) { //check if the movie is already in favorites
+            if ((m.imdbID === movie.imdbID)) {
+                isFav = true;
+                addToFavBtn.removeEventListener("mouseout", mouseOutOnFav)
+                addToFavBtn.classList.remove("fa-regular");
+                addToFavBtn.classList.add("fa-solid");
+                addToFavBtn.setAttribute("title", "Remove from favorites");
+            }
+        }
+
+        addToFavBtn.addEventListener("click", onClickingFav)
+        function onClickingFav() {
+            if (!isFav) { // if the movie is not in favorites
+                alert("Added to favorites")
+                // CHANGING BUTTON APPEARANCE
+                addToFavBtn.removeEventListener("mouseout", mouseOutOnFav)
+                addToFavBtn.classList.remove("fa-regular");
+                addToFavBtn.classList.add("fa-solid");
+
+                // ACTUALLY ADDING TO FAVORITES
+                favorites.push(movie);
+                localStorage.setItem("favoriteMovies", JSON.stringify(favorites));
+            }
+            else { // if the movie is already in favorites
+                // CHANGING BUTTON APPEARANCE
+                addToFavBtn.addEventListener("mouseout", mouseOutOnFav)
+                addToFavBtn.classList.add("fa-regular");
+                addToFavBtn.classList.remove("fa-solid");
+
+                // ACTUALLY REMOVING FROM FAVORITES
+                favorites = favorites.filter(m => movie.imdbID !== m.imdbID);
+                console.log(favorites);
+                localStorage.setItem("favoriteMovies", JSON.stringify(favorites));
+            }
+            isFav = !isFav;
+        }
+    }
+    handleFavBtn()
 
 })();
